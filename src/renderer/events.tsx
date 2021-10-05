18a243f191cd43868d661ed4eb6eac3ee5bd2280
Nova1545 <aidencw01@gmail.com>
2021-10-04 20:55:22 -0400

    Basic Websocket implementation

--- src/renderer/events.tsx
+++ src/renderer/events.tsx
@@ -1,10 +1,44 @@
 import { ipcRenderer, Navigate } from './helpers';
 import { LoadMessageFeed } from './pages/Chat/index';
 
+function getCookie(cname: string) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+let socket : WebSocket;
 ipcRenderer.on('end_auth', (data: boolean) => {
   if (data) {
     Navigate("/chat");
     ipcRenderer.send('requestChannelData', 'b1642a0175554994b3f593f191c610b5');
+    console.log(getCookie("userData"));
+    const { token, uuid } = JSON.parse(getCookie("userData"));
+    //socket = new WebSocket(`wss://localhost:44365/Events/Listen?user_uuid=${uuid}`)  
+    socket = new WebSocket(`wss://api.novastudios.tk/Events/Listen?user_uuid=${uuid}`)
+    socket.onmessage = function (message) {
+      console.log(message);
+    };
+    socket.onerror = function (error) {
+      console.error(error);
+    };
+    socket.onopen = function () {
+      socket.send("Test");
+    };
+    socket.onclose = function (event) {
+      console.log(event);
+    }
+    return true;
   }
 });
 
