2ae98023df9bd49a6301b1d6482ab409e825f264
Nova1545 <aidencw01@gmail.com>
2021-10-06 17:54:54 -0400

    Only request required image size

--- src/renderer/pages/Chat/index.tsx
+++ src/renderer/pages/Chat/index.tsx
@@ -86,7 +86,7 @@ export default class Chat extends React.Component {
       let message = messages[index];
       // Testing purposes
       if (canvas != null) {
-        const msgObj = new Message({ message: message.content, author: message.author, avatarSrc: `https://api.novastudios.tk/Media/Avatar/${message.author_UUID}` });
+        const msgObj = new Message({ message: message.content, author: message.author, avatarSrc: `https://api.novastudios.tk/Media/Avatar/${message.author_UUID}?size=64` });
         canvas.append(msgObj);
       }
       else {
