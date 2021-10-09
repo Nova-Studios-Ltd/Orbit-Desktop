367316ed6a5649ab6386140e638c7be64ee601a5
GentlyTech <andyyep@hotmail.com>
2021-10-09 15:05:05 -0400

    Began adding channel view (buggy)

---
+++ src/renderer/pages/Chat/ChannelView.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ChannelView extends React.Component {
+  constructor(props) {
+    super(props);
+}
+
+  render() {
+    return(
+      <div className="Chat_Page_ChannelView">
+
+      </div>
+    );
+  }
+}
