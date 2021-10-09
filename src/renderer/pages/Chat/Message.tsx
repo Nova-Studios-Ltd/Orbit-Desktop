853b713e766bebe93de8e0f9872ee65f8ed99594
Nova1545 <aidencw01@gmail.com>
2021-10-09 14:47:42 -0400

    Maybe this works better for image embeding?

--- src/renderer/pages/Chat/Message.tsx
+++ src/renderer/pages/Chat/Message.tsx
@@ -50,11 +50,22 @@ export default class Message extends React.Component {
       this.divRef.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "start"});
   }
 
+  validURL(str: string) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
   render() {
-    const urlSearchPattern ="([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?";
+    /*const urlSearchPattern ="([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?";
     const matches = this.message.match(urlSearchPattern);
     let messageContentObject = null;
     if (matches != null) {
+      console.log(matches);
       const filteredMatch = matches[0].split(" ")[0];
       const filteredMessage = this.message.replace(filteredMatch, "");
       console.log(`Match: ${filteredMatch}`);
@@ -62,7 +73,14 @@ export default class Message extends React.Component {
     }
     else {
       messageContentObject = <Typography className="Chat_Message_Content">{this.message}</Typography>;
-    }
+    }*/
+
+    let messageContentObject = [] as any;
+    const message = this.message.split(/( |\n)/g);
+    message.forEach(word => {
+      if (this.validURL(word)) messageContentObject.push(<MessageImage message={word} src={word} />)
+    });
+    if (messageContentObject.length == 0) messageContentObject.push(<Typography className="Chat_Message_Content">{this.message}</Typography>);
 
     return (
       <div className="Chat_Message" ref={this.divRef}>
