6c329b23d72c9089eddb4b602105843666fe4d40
Nova1545 <aidencw01@gmail.com>
2021-10-17 18:05:33 -0400

    Fixed Image Embeding and Links

--- src/renderer/pages/Chat/Message.tsx
+++ src/renderer/pages/Chat/Message.tsx
@@ -60,7 +60,7 @@ export default class Message extends React.Component {
   }
 
   imageURL(url: string) {
-    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+    return(url.match(/\.(jpeg|jpg|gif|png|webp)$/) != null);
   }
 
   deleteMessage() {
@@ -83,14 +83,49 @@ export default class Message extends React.Component {
     }*/
 
     let messageContentObject = [] as any;
-    const message = this.message.split(/( |\n)/g);
-    var first = true;
+    let content = this.message.split(/(https:\/\/[a-zA-Z0-9_\-\.\/]*)/g);
+    let links = this.message.match(/(https:\/\/[a-zA-Z0-9_\-\.\/]*)/g);
+    let containsText = false;
+    content.forEach(word => {
+      if (!this.validURL(word) && word != "") containsText = true;
+    });
+
+    if (containsText) {
+      const mes = this.message.split(/(https:\/\/[a-zA-Z0-9_\-\.\/]*)/g);
+      var messageParts = [] as any[];
+      console.log(mes);
+      mes.forEach(word => {
+        if (this.validURL(word)) messageParts.push(<Link target="_blank" href={word}>{word}</Link>);
+        else messageParts.push(word);
+      });
+
+      messageContentObject.push(<Typography className="Chat_Message_Content">{messageParts}</Typography>);
+    }
+
+    if (links != null && links.length > 0) {
+      links.forEach(link => {
+        if (this.imageURL(link)) messageContentObject.push(<MessageImage key={link} message={link} src={link} />);
+      });
+    }
+
+
+    /*var first = true;
     message.forEach(word => {
       var m = (first)? this.message.replaceAll(word, "") : "";
       if (this.validURL(word) && this.imageURL(word)) messageContentObject.push(<MessageImage key={word} message={word} src={word} />)
       first = false;
     });
-    if (messageContentObject.length == 0) messageContentObject.push(<Typography className="Chat_Message_Content">{this.message}</Typography>);
+    if (messageContentObject.length == 0) {
+      const mes = this.message.split(/(https:\/\/[a-zA-Z\.\/]*)/g);
+      var messageParts = [] as object[];
+      console.log(mes);
+      mes.forEach(word => {
+        if (this.validURL(word)) messageParts.push(<Link target="_blank" href={word}>{word}</Link>);
+        else messageParts.push(word);
+      });
+
+      messageContentObject.push(<Typography className="Chat_Message_Content">{messageParts}</Typography>);
+    }*/
 
     return (
       <div className="Chat_Message" ref={this.divRef}>
