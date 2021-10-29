baec13a04838890ea3b2017805103dcbd85884c5
GentlyTech <andyyep@hotmail.com>
2021-10-28 20:25:44 -0400

    Animated channel click

--- src/renderer/components/Channels/Channel.tsx
+++ src/renderer/components/Channels/Channel.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import { Card, Typography, Avatar, CardMedia } from '@mui/material';
 import GLOBALS from 'renderer/globals'
 import { ipcRenderer, LoadMessageFeed, setDefaultChannel } from 'renderer/helpers';
 import { IChannelProps } from 'renderer/interfaces';
+import TouchRipple from '@mui/material/ButtonBase/TouchRipple';
 
 export default class Channel extends React.Component {
   channelName: string;
   channelID: string;
   channelIcon?: string;
+  rippleRef: RefObject;
 
   constructor(props: IChannelProps) {
     super(props);
@@ -15,7 +17,11 @@ export default class Channel extends React.Component {
     this.channelID = props.channelID;
     this.channelIcon = props.channelIcon;
 
+    this.rippleRef = React.createRef();
+
     this.channelClicked = this.channelClicked.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
   }
 
   channelClicked() {
@@ -25,9 +31,22 @@ export default class Channel extends React.Component {
     ipcRenderer.send('requestChannelData', GLOBALS.currentChannel);
   }
 
+  onMouseDown(event: React.MouseEvent<HTMLDivElement> ) {
+    if (this.rippleRef != null && this.rippleRef.current != null) {
+      this.rippleRef.current.start(event);
+    }
+  }
+
+  onMouseUp(event: React.MouseEvent<HTMLDivElement> ) {
+    if (this.rippleRef != null && this.rippleRef.current != null) {
+      this.rippleRef.current.stop(event);
+    }
+  }
+
   render() {
     return(
-      <Card className='Chat_Channel' onClick={this.channelClicked}>
+      <Card className='Chat_Channel' onClick={this.channelClicked} onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp}>
+        <TouchRipple ref={this.rippleRef}/>
         <div className='Channel_Left'>
           <Avatar className='Channel_Icon' src={this.channelIcon} />
         </div>
