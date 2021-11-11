89bd4934daf1ed34e558cfb9f17a2b9883b35827
Nova1545 <aidencw01@gmail.com>
2021-11-10 22:24:09 -0500

    Implemented Random Gradients

--- src/renderer/pages/Auth/index.tsx
+++ src/renderer/pages/Auth/index.tsx
@@ -15,6 +15,29 @@ function RegisterInit(form: RegisterForm) {
 
 }
 
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+// Random Experiemnt stuff
+function GenerateRandomColor() {
+  var r = getRandomInt(0, 255);
+  var g = getRandomInt(0, 255);
+  var b = getRandomInt(0, 255);
+  return rgbToHex(r, g, b);
+}
+
+function componentToHex(c: number) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r: number, g: number, b: number) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 export default class AuthPage extends React.Component {
   formType: Number;
 
@@ -34,8 +57,12 @@ export default class AuthPage extends React.Component {
     const background = this.formType == 1 ? 'Register_Page_Container_Type' : 'Login_Page_Container_Type' ;
     const AuthPageContainerClassNames = `Auth_Page_Container ${background}`
 
+    const st = {
+      backgroundImage: `linear-gradient(43deg, ${GenerateRandomColor()} 0%, ${GenerateRandomColor()} 46%, ${GenerateRandomColor()} 100%)`,
+    };
+
     return (
-      <div className={AuthPageContainerClassNames}>
+      <div className={AuthPageContainerClassNames} style={st}>
         <Helmet>
           <title>{title}</title>
         </Helmet>
