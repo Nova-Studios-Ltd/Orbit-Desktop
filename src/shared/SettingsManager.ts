e68ec01dbf55015e9aebd8d079d28f19864f369f
GentlyTech <andyyep@hotmail.com>
2021-11-22 22:35:39 -0500

    Added outline for settings

---
+++ src/shared/SettingsManager.ts
@@ -0,0 +1,13 @@
+// Figure out how to store settings (e.g. json file)
+// Decide whether or not to store/retrieve settings in memory or to just read/write straight to the file at all times
+// Figure out how settings can be interfaced by the client
+
+// Settings
+/*
+
+  - Theme
+  - Notification Type
+  - Accessibility options
+    - Reduced motion/no animations
+
+*/
